5770fa37af9bb6d5e18c1a80057b1de4413f8fb3
Anton <gubernatorov.tosha@yandex.ru>
2023-05-07 19:39:18 +0300

    add build

---
+++ config/build/types/config.ts
@@ -0,0 +1,19 @@
+export type buildMode = "production" | "development";
+
+export interface BuildPaths {
+  entry: string;
+  build: string;
+  html: string;
+}
+
+export interface BuildEnv {
+  mode: buildMode;
+  port: number;
+}
+
+export interface BuildOptions {
+  mode: buildMode;
+  paths: BuildPaths;
+  isDev: boolean;
+  port: number;
+}
