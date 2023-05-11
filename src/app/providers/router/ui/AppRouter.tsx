061ff3cc1011ec27d8f2635375042b891b2f749d
Anton <gubernatorov.tosha@yandex.ru>
2023-05-11 19:22:02 +0300

    loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,10 +2,11 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routerConfig/routerConfig";
 import { classNames } from "shared/lib/ClassNames/classNames";
+import PageLoader from 'shared/ui/PageLoader/PageLoader';
 
 const router = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader/>}>
       <div className={classNames("page-wrapper", {}, [])}>
         <Routes>
           {Object.values(routeConfig).map(({ element, path }) => (
---
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -0,0 +1,27 @@
+import { classNames } from 'shared/lib/ClassNames/classNames';
+import './PageLoader.scss'
+
+const PageLoader = () => {
+	return (
+    <div className={classNames("PageLoader", {}, [])}>
+      <div className="loadingio-spinner-spinner-svmgjmvd39r">
+        <div className="ldio-95bb8429dx">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default PageLoader;
